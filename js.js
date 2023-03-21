a35dbc22f3ce7091ebf9899848d96a44494235ec
ILegresti <ignaciolegresti2@gmail.com>
2023-03-20 23:32:10 -0300

    simulador terminado

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-/* let totalDelIntercambio = 0;
+let totalDelIntercambio = 0;
 const valorDolar = 373;
 const valorEuro = 212;
 const valorPesoArg = 1;
@@ -16,20 +16,41 @@ function totalDeLaConversion (cantidadAIntercambiar){
     return alert("El total del intercambio es: "+ totalDelIntercambio)
 }
 
-totalDeLaConversion(parseFloat(prompt("ingrese cantidad a intercambiar:")))
+/* totalDeLaConversion(parseFloat(prompt("ingrese cantidad a intercambiar:")))
+
+
  */
 
+
+
 let totalHastaAhora = 0;
-let mensajeUsuario = prompt("Ingrese cantidad a intercambiar o presione 0 para salir")
-function sumoDeAUnoHastaLLegarACantidad(cantidadASumar){
-    while(totalHastaAhora != cantidadASumar && mensajeUsuario != "0"){
-        totalHastaAhora = totalHastaAhora + mensajeUsuario
-        mensajeUsuario = prompt("Ingrese cantidad a intercambiar o presione 0 para salir")
+function convertir(cantidad, divisa){
+    if(divisa == "Dolar" || divisa == "dolar"){
+        totalHastaAhora = cantidad / valorDolar
+    }else if(divisa == "Euro" || divisa == "euro"){
+        totalHastaAhora = cantidad / valorEuro
+    }else if(divisa == "peso arg" || divisa == "Peso arg"){
+        totalHastaAhora = cantidad * valorPesoArg
+    }else{
+        alert("Moneda aun no identificada ")
     }
-    return console.log("Tu total es: "+ totalHastaAhora)
+    return(totalHastaAhora)
 }
 
-sumoDeAUnoHastaLLegarACantidad(mensajeUsuario) 
+
+let totalDeIntercambio = 0;
+let mensajeUsuario = prompt("Ingrese cantidad a intercambiar o presione 0 para salir")
+function sumoDeAUnoHastaLLegarACantidad(){
+    while(mensajeUsuario != "0"){
+        const divisa = prompt("Ingrese la moneda por la cual quiere intercambiar sus divisas: ");
+        const conversion = convertir(mensajeUsuario, divisa);
+        alert("Resultado de conversion: "+ conversion);
+        mensajeUsuario = prompt("Ingrese cantidad a intercambiar o presione 0 para salir")
+        totalDeIntercambio = totalDeIntercambio + conversion; 
+    }
+    return(alert("Total convertido: "+ totalDeIntercambio))
+}
 
 
+sumoDeAUnoHastaLLegarACantidad()
 
